56d34fca0dec8a1957cea7675b209f9cc0ea3e8f
Anna Gabriela <annagmcarvalho@gmail.com>
2020-06-16 08:48:29 -0300

    add beer information reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import beerHouseReducer from '../app/pages/beerhouse/BeerHouseSlice'
+import beerInformationReducer from '../app/pages/beerInformation/BeerInformationSlice'
 
 export default configureStore({
   reducer: {
-    beerHouse: beerHouseReducer
+    beerHouse: beerHouseReducer,
+    beerInformation: beerInformationReducer
   },
 });
